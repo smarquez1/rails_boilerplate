ed9de283fd28f3412c1649083f59de17ac6ce9ac
Sergio D. Márquez <marquez.sergio.d@gmail.com>
2019-05-03 19:21:39 +0200

    Load scss/images through webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,11 @@
 import Turbolinks from 'turbolinks'
+import 'lazysizes'
+import 'lazysizes/plugins/parent-fit/ls.parent-fit'
 // import Activestorage from '@rails/activestorage'
+// Activestorage.start()
 
 import '../controllers'
+import '../images/avatar.png'
+import '../src/application.scss'
 
 Turbolinks.start()
-// Activestorage.start()
